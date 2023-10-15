5856962a212169a2faad4372014243264d61a9eb
Breno Battaglin <breno.battaglin@icloud.com>
2023-10-15 00:51:44 -0300

    style: Create font values

---
+++ lib/config/fonts.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class FontConstants {
+  static const String fontFamily = "Raleway";
+}
+
+class FontWeights {
+  static const FontWeight thin = FontWeight.w100;
+  static const FontWeight extraLight = FontWeight.w200;
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semibold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight extraBold = FontWeight.w800;
+  static const FontWeight black = FontWeight.w900;
+}
+
+class FontSizes {
+  static const double small = 12.0;
+  static const double medium = 14.0;
+  static const double large = 16.0;
+  static const double extraLarge = 18.0;
+  static const double heading = 22.0;
+  static const double title = 26.0;
+}
