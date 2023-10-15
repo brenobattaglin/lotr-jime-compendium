8caee1970ba0b26b1895f1a4217e29b5cba25a4a
Breno Battaglin <breno.battaglin@icloud.com>
2023-10-15 00:51:10 -0300

    styie: Create app colors

---
+++ lib/config/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFb79c71);
+  static const Color darkGrey = Color(0xFF363636);
+  static const Color grey = Color(0xFF808080);
+  static const Color lightGrey = Color(0xFFa2a2a2);
+
+  static const Color darkPrimary = Color(0xFF402f21);
+  static const Color grey1 = Color(0xFF707070);
+  static const Color grey2 = Color(0xFF797979);
+  static const Color white = Color(0xFFe8e8e8);
+  static const Color error = Color(0xFFe61f34);
+}
