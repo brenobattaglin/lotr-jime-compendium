a481d590747c7ea50b6b5fe0ddfc348d2d3f38c9
Breno Battaglin <breno.battaglin@icloud.com>
2023-10-21 11:13:22 -0300

    chore: Add castle animation

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -3,3 +3,13 @@ const String _imagePath = 'assets/images';
 class AppImageAssets {
   static const String splashLogo = "$_imagePath/splash_logo.png";
 }
+
+class AppLottieAssets {
+  static const String castle = "$_lottiePath/castle.json";
+}
+
+class AppLottieAssetSize {
+  static const double small = 100.0;
+  static const double medium = 200.0;
+  static const double large = 300.0;
+}
