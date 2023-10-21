998e51bb397ffc2f1ee01d00e4a2d72a87a1c12e
Breno Battaglin <breno.battaglin@icloud.com>
2023-10-20 22:28:29 -0300

    feat: Create splash view

---
+++ lib/presentation/splash/splash_view.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:jime_compendium/config/config.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer?.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) => _buildScaffold();
+
+  Widget _buildScaffold() => const Scaffold(
+        backgroundColor: AppColors.darkPrimary,
+        body: Center(
+          child: Image(
+            image: AssetImage(AppImageAssets.splashLogo),
+          ),
+        ),
+      );
+
+  _startDelay() {
+    _timer = Timer(const Duration(seconds: 2), _pushToOnboardingView);
+  }
+
+  _pushToOnboardingView() => Navigator.pushReplacementNamed(context, Routes.onboardingRoute);
+}
