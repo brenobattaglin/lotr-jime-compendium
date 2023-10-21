5086c48502154f159c6821a6e917e2de8043cb5d
Breno Battaglin <breno.battaglin@icloud.com>
2023-10-20 22:09:30 -0300

    chore: Create onboarding route

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:jime_compendium/presentation/resources/strings.dart';
+import 'package:jime_compendium/config/strings.dart';
 
 class Routes {
   static const String splashRoute = '/';
+  static const String onboardingRoute = '/onboarding';
 }
 class RouteGenerator {
   static Route<MaterialPageRoute> getRoute(RouteSettings routeSettings) {
