9815877c5ea2345113df62c6b6ea514518e8dcdb
Breno Battaglin <breno.battaglin@icloud.com>
2024-03-01 21:53:12 -0300

    chore: update import and remove old widget

--- lib/features/onboarding/presentation/presentation.dart
+++ lib/features/onboarding/presentation/presentation.dart
@@ -1,4 +1,4 @@
 export 'onboarding_slide_content.dart';
 export 'onboarding_view.dart';
 export 'onboarding_view_driver.dart';
-export 'onboarding_view_indicator.dart';
+export 'onboarding_slide_indicator.dart';
